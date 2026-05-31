4af7f39ccac66e40331ad713baa81149f2b338e1
Basti Autoplay <paduacharis@gmail.com>
2026-06-01 03:27:54 +0800

    Added DBConnection class - MySQL database connection setup

---
+++ HRApplicantSystem/Database/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+
+namespace HRApplicantSystem.Database
+{
+    public class DBConnection
+    {
+        // ⚠️ Change "Admin1234" to YOUR MySQL root password
+        private static string connectionString =
+            "Server=localhost;Database=hr_applicant_system;Uid=root;Pwd=WgPtMaCgR2;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
