f5869e6376a8457e2b5785635f74bc27c1e8665b
Basti Autoplay <paduacharis@gmail.com>
2026-06-01 14:25:36 +0800

    Added Login screen UI and login logic for HR and Applicant accounts

--- HRApplicantSystem/LoginForm.Designer.cs
+++ HRApplicantSystem/LoginForm.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(37, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(357, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HR Applicant System";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblSubtitle
+            // 
+            this.lblSubtitle.AutoSize = true;
+            this.lblSubtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtitle.Location = new System.Drawing.Point(105, 49);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(198, 25);
+            this.lblSubtitle.TabIndex = 1;
+            this.lblSubtitle.Text = "Login to your account";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(8, 79);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(125, 22);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email Address";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(13, 149);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(89, 22);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.Click += new System.EventHandler(this.lblPassword_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(17, 104);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(350, 26);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 192);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(350, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(91, 233);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(212, 30);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnRegister.Location = new System.Drawing.Point(75, 280);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(268, 34);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "New Applicant? Register Here";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(428, 344);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblSubtitle);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HR Applicant System - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
 
--- HRApplicantSystem/LoginForm.cs
+++ HRApplicantSystem/LoginForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using HRApplicantSystem.Database;
 
 namespace HRApplicantSystem
 {
@@ -15,6 +17,113 @@ namespace HRApplicantSystem
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            // Check for empty fields
+            if (email == "" || password == "")
+            {
+                MessageBox.Show("Please enter your email and password.",
+                                "Warning", MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                MySqlConnection conn = DBConnection.GetConnection();
+                conn.Open();
+
+                // Check HR/Admin Users table first
+                string hrQuery = "SELECT UserID, FullName, RoleID FROM Users " +
+                                 "WHERE Email=@email AND PasswordHash=@password " +
+                                 "AND IsActive=1";
+
+                MySqlCommand hrCmd = new MySqlCommand(hrQuery, conn);
+                hrCmd.Parameters.AddWithValue("@email", email);
+                hrCmd.Parameters.AddWithValue("@password", password);
+
+                MySqlDataReader hrReader = hrCmd.ExecuteReader();
+
+                if (hrReader.Read())
+                {
+                    string fullName = hrReader["FullName"].ToString();
+                    int roleID = Convert.ToInt32(hrReader["RoleID"]);
+                    hrReader.Close();
+                    conn.Close();
+
+                    MessageBox.Show("Welcome, " + fullName + "!",
+                                   "Login Successful",
+                                   MessageBoxButtons.OK,
+                                   MessageBoxIcon.Information);
+
+                    // We'll open HR Dashboard here later
+                    return;
+                }
+
+                hrReader.Close();
+
+                // If not HR, check Applicant accounts
+                string appQuery = "SELECT ApplicantAccountID, Email FROM ApplicantAccounts " +
+                                  "WHERE Email=@email AND PasswordHash=@password " +
+                                  "AND IsActive=1";
+
+                MySqlCommand appCmd = new MySqlCommand(appQuery, conn);
+                appCmd.Parameters.AddWithValue("@email", email);
+                appCmd.Parameters.AddWithValue("@password", password);
+
+                MySqlDataReader appReader = appCmd.ExecuteReader();
+
+                if (appReader.Read())
+                {
+                    int accountID = Convert.ToInt32(appReader["ApplicantAccountID"]);
+                    appReader.Close();
+                    conn.Close();
+
+                    MessageBox.Show("Welcome back!",
+                                   "Login Successful",
+                                   MessageBoxButtons.OK,
+                                   MessageBoxIcon.Information);
+
+                    // We'll open Applicant Dashboard here later
+                    return;
+                }
+
+                appReader.Close();
+                conn.Close();
+
+                // If neither matched
+                MessageBox.Show("Invalid email or password.",
+                               "Login Failed",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database error: " + ex.Message,
+                               "Error", MessageBoxButtons.OK,
+                               MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // We'll open Registration form here later
+            MessageBox.Show("Registration form coming soon!", "Info");
         }
     }
 }
